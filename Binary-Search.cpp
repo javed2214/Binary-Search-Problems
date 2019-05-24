f238cc46ddbf786991afc0f38d4ebf185b9acccf
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-05-24 19:07:01 +0530

    Update Binary-Search.cpp

--- Binary-Search.cpp
+++ Binary-Search.cpp
@@ -3,32 +3,45 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int binarySearch(int *a, int start, int end, int x){
-
-	if(start <= end){
-
-		int mid=(start+end)/2;
-		if(a[mid]==x) return mid;
-		if(x<a[mid]) return binarySearch(a,start,mid-1,x);
-		if(x>a[mid]) return binarySearch(a,mid+1,end,x);
+int binarySearch(int *a, int s, int e, int x){
+	
+	if(e>=s){
+		
+		int mid=(s+e)/2;
+		
+		if(a[mid]==x)
+			return mid;
+		if(x>a[mid])
+			return binarySearch(a,mid+1,e,x);
+		else
+			return binarySearch(a,s,mid-1,x);
 	}
+
 	return -1;
 }
 
-int main(){
+/*
+*********** ITERATIVE BINARY SEARCH ***********
 
+int binarySearch(int *a, int start, int end, int x){
+
+	while(start <= end){
+		int mid=(start + end)/2;
+		if(x==a[mid]) return mid;
+		if(x>a[mid]) start=mid+1;
+		if(x<a[mid]) end=mid-1;
+	}
+	return -1;
+}
+*/
+
+
+int main(){
 	int a[]={1,2,3,4,5,6,7,8,9,10};
 	int n=sizeof(a)/sizeof(int);
-
-	int x=10;
-
+	int x=1;
 	int p=binarySearch(a,0,n-1,x);
-	
-	if(p!=-1) cout<<"Number Found at Index "<<p<<"\n";
-	else cout<<"Number Not Found!\n";
-
-	// Inbuilt Binary Search Function
-	if(binary_search(a,a+n,x)) cout<<"Found!";
+	cout<<"Index: "<<p;
 
 	return 0;
 }

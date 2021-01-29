09ebda469ed271da44219824c46c16843053ef7a
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2021-01-29 20:48:56 +0530

    Update Min-Element-In-Rotated-Sorted-Array.cpp

--- Min-Element-In-Rotated-Sorted-Array.cpp
+++ Min-Element-In-Rotated-Sorted-Array.cpp
@@ -20,7 +20,7 @@ int findMin(int *a, int x, int y){
 		int prev = (mid - 1 + n) % n;	// +n is used to Avoid Underflow
 		if(a[mid] <= a[prev] and a[mid] <= a[next]) return mid;
 		else if(a[mid] >= a[l]) l = mid + 1;
-		else if(a[mid] <= a[r]) r = mid - 1;
+		else r = mid - 1;
 	}
 	return -1;
 }

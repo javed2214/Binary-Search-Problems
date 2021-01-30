05605e7da166a8091bdc854b4cc91027b96d2bc2
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2021-01-30 23:10:45 +0530

    Update Search-In-Circular-Sorted-Array.cpp

--- Search-In-Circular-Sorted-Array.cpp
+++ Search-In-Circular-Sorted-Array.cpp
@@ -1,32 +1,32 @@
 // Program to Search Element in Circular Sorted Array
+// https://leetcode.com/problems/search-in-rotated-sorted-array/submissions/
 
-#include<bits/stdc++.h>
-using namespace std;
+// Input: nums = [4,5,6,7,0,1,2], target = 0
+// Output: 4
 
-int searchElement(int *a, int start, int end, int x){
-
-	while(start <= end){
-
-		int mid=(start + end)/2;
-		if(a[mid]==x) return mid;
-		if(a[mid] <= a[end]){
-			if(x > a[mid] and x <= a[end]) start = mid + 1;
-			else end = mid - 1;
-		}
-		else{
-			if(x >= a[start] and x < a[mid]) end = mid - 1;
-			else start = mid + 1;
-		}
-	}
-	return -1;
-}
-
-int main(){
-
-	int a[]={5,6,7,1,2,3,4};
-	int n=sizeof(a)/sizeof(int);
-
-	cout<<"Index: "<<searchElement(a,0,n-1,1);
-
-	return 0;
-}
+class Solution {
+public:
+    
+    int go(vector <int> &A, int k){
+        
+        int start = 0, end = A.size() - 1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(A[mid] == k) return mid;
+            if(A[mid] > A[end]){
+                if(k >= A[start] and k < A[mid]) end = mid - 1;
+                else start = mid + 1;
+            }
+            else{
+                if(k > A[mid] and k <= A[end]) start = mid + 1;
+                else end = mid - 1;
+            }
+        }
+        return -1;
+    }
+    
+    int search(vector<int>& nums, int target) {
+        
+        return go(nums, target);
+    }
+};
